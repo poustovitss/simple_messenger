34036623e6bb3c2ba0e69cd8f64413a8252317a9
Sergiy Poustovit <levellevel4@gmail.com>
2017-04-28 15:15:14 +0300

    Time zone added

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module SimpleMessenger
   class Application < Rails::Application
+    config.time_zone = 'Kyiv'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
